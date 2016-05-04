b2c3d65b580c47bb4716a3cd480f5f90c0fe745a
codeR <coder@coder.com>
2016-05-04 17:18:16 +0530

    Fixed Vendor

--- note_3g-vendor.mk
+++ note_3g-vendor.mk
@@ -1,8 +1,8 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/redmi/note_3g/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/note_3g/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/redmi/note_3g/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/note_3g/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/redmi/note_3g/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/note_3g/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/redmi/note_3g/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/note_3g/proprietary/xbin,system/xbin)
 
